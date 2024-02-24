3e05fe4866ccfa8de55a0ffcbebbf4189ac6be56
szserrano <szserrano@mail.csuchico.edu>
2024-02-24 02:59:51 -0800

    Templates going to Conditional Statements

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # AutoPoster
+
 This is a tool that automates posting on several social media platforms at the same while providing analytics and
 a database of posts. The first goal is to create a tool to post on Facebook account with text, hashtags, photos, and videos
+
+## Note to self:
+
+If receiving this error..
+Error: Could not import 'app.autoposter'.
+..make sure that you are in correct directory with autoposter.py
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 # This file is used to route each url request to a 'view' function module defined in the app package directory
+from flask import render_template
 from app import app
 
 # These decorators attached above the function attach function to these URLS
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Sydney'}
+    return render_template('index.html', title='Home', user=user)
