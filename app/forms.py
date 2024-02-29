b936904ac256163daf2717c478f8e74cf316507f
szserrano <szserrano@mail.csuchico.edu>
2024-02-28 23:57:45 -0800

    Move onto Chapter 4: Database

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+# Imported FlaskForm class from flask_wtf package
+# Imported Field classes from wtforms package
+# Imported DataRequired function from wtforms.validators package to verify nonempty fields
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # This file is used to route each url request to a 'view' function module defined in the app package directory
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 # These decorators attached above the function attach function to these URLS
 @app.route('/')
@@ -18,3 +19,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
