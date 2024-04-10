0309a28b9b5bbf24f62c85b44c1f0850a2ad5461
szserrano <szserrano@mail.csuchico.edu>
2024-04-09 21:10:44 -0700

    Merge and move to Chapter 7

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@
 # Imported Field classes from wtforms package
 # Imported DataRequired function from wtforms.validators package to verify nonempty fields
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -33,3 +33,8 @@ class RegistrationForm(FlaskForm):
             User.email == email.data))
         if user is not None:
             raise ValidationError('Please use a different email address.')
+        
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
