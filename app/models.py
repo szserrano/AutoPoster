9388ef7acdea5b9534fa0f29d9f588504167e36e
szserrano <szserrano@mail.csuchico.edu>
2024-04-18 15:56:14 -0700

    Revert "Revert "Added Password Reset Tokens. Continue to Sending a Password Reset Email""
    This reverts commit 3916910a576f5401bee01d34a9026f8787b6c571.

--- app/models.py
+++ app/models.py
@@ -15,9 +15,11 @@ from flask_login import UserMixin
 from datetime import datetime, timezone
 from typing import Optional
 from hashlib import md5
+from time import time
+import jwt
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import app, db, login
 
 followers = sa.Table(
     'followers',
@@ -85,6 +87,21 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256'
+        )
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
--- config.py
+++ config.py
@@ -14,5 +14,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['sjzserrano@gmail.com']
+    ADMINS = ['sydbrawrdude@gmail.com', 'sjzserrano@gmail.com']
     POSTS_PER_PAGE = 10
